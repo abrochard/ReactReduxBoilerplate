5c0293f057835aaf4f1290dc3de60dde0bc9ac50
adrien <adrien@arch-virtualbox>
2018-02-10 18:12:58 -0500

    adding redux-promise

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const path = require('path');
 const app = express();
 
 app.use(express.static('public'));
-app.get('/*', (req, res) => res.sendFile(path.join(__dirname, '../public', 'index.html')));
+app.get('/*', (req, res) => res.sendFile(path.join(__dirname, '/public', 'index.html')));
 
 app.listen(3000, () => console.log('Listening on port 3000'));
