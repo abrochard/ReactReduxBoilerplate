ff91adb9c0ebc83f402070cb7e0d47d079e02820
Adrien Brochard <aaa.brochard@gmail.com>
2018-02-18 12:02:11 -0500

    adding style/css loader

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,14 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      },
+      {
+        test: /\.(png|gif|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000'
       }
     ]
   },
